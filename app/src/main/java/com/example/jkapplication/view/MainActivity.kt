237cc01af29babdb77580801af32d6bd43932109
smtownaosdev2 <smtown.aos.dev2@gmail.com>
2020-07-22 12:19:18 +0900

    Init

---
+++ app/src/main/java/com/example/jkapplication/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jkapplication.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jkapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
