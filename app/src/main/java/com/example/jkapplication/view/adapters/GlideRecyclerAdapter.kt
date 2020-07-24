2c834a272436dae6f0f8bdabd977057135a1154b
smtownaosdev2 <smtown.aos.dev2@gmail.com>
2020-07-24 11:38:59 +0900

    Add glide

--- app/src/main/java/com/example/jkapplication/view/adapters/GlideRecyclerAdapter.kt
+++ app/src/main/java/com/example/jkapplication/view/adapters/GlideRecyclerAdapter.kt
@@ -1,13 +1,17 @@
 package com.example.jkapplication.view.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.jkapplication.R
 import com.example.jkapplication.model.Monster
+import com.example.jkapplication.view.fragments.GlideFragment
 
 class GlideRecyclerAdapter(context: Context, list: ArrayList<Monster>) :
     RecyclerView.Adapter<GlideRecyclerAdapter.Holder>() {
@@ -28,6 +32,8 @@ class GlideRecyclerAdapter(context: Context, list: ArrayList<Monster>) :
     override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.title.text = list[position].title
         holder.date.text = list[position].date
+        Glide.with(holder.image.context).load(list[position].img_url).override(500,500).centerInside().into(holder.image)
+
     }
 
     fun replaceAll(items: ArrayList<Monster>) {
@@ -48,7 +54,9 @@ class GlideRecyclerAdapter(context: Context, list: ArrayList<Monster>) :
     class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var title: TextView = itemView.findViewById(R.id.v_glide_tv_title)
         var date: TextView = itemView.findViewById(R.id.v_glide_tv_date)
+        var image: ImageView = itemView.findViewById(R.id.v_glide_iv_image)
     }
+
 
 
 }
