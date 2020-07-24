db4072f1e3d8fb24da8987fd3d47a2683d4564db
smtownaosdev2 <smtown.aos.dev2@gmail.com>
2020-07-24 15:05:44 +0900

    Add Refreshing

--- app/src/main/java/com/example/jkapplication/view/fragments/GlideFragment.kt
+++ app/src/main/java/com/example/jkapplication/view/fragments/GlideFragment.kt
@@ -2,13 +2,16 @@ package com.example.jkapplication.view.fragments
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.jkapplication.R
 import com.example.jkapplication.data.ImagesResponse
 import com.example.jkapplication.data.network.GlideRetrofit
@@ -21,10 +24,12 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class GlideFragment : Fragment(){
+class GlideFragment : Fragment() {
     lateinit var recyclerView: RecyclerView
     lateinit var list: ArrayList<Monster>
-    lateinit var adapter:GlideRecyclerAdapter
+    lateinit var adapter: GlideRecyclerAdapter
+    lateinit var swipeRefreshLayout:SwipeRefreshLayout
+    lateinit var ft:FragmentTransaction
 
     var retrofit = GlideRetrofit()
     var imglist = retrofit.response
@@ -33,7 +38,7 @@ class GlideFragment : Fragment(){
 
         imglist.enqueue(object : Callback<ImagesResponse?> { //enqueue랑 속도가 안맞아서 동기화 해야
             override fun onFailure(call: Call<ImagesResponse?>, t: Throwable) {
-                Log.e("glide presenter", "fail")
+                Log.e("glide fragment", "fail")
             }
 
             override fun onResponse(
@@ -41,9 +46,9 @@ class GlideFragment : Fragment(){
                 response: Response<ImagesResponse?>
             ) {
                 val body = response.body()
-                Log.e("glide presenter", "ok")
+                Log.d("glide fragment", "ok")
                 list = body!!.images
-                Log.d("item check",list[0].date)
+                Log.d("item check", list[0].date)
                 adapter.replaceAll(list)
             }
 
@@ -59,24 +64,37 @@ class GlideFragment : Fragment(){
         list = createContactsList(5)//demo list
         recyclerView.setHasFixedSize(true)
 
-        var context:Context = this!!.activity!!
-        adapter = GlideRecyclerAdapter(context,list) //context호출 맞나....
+        var context: Context = this!!.activity!!
+        adapter = GlideRecyclerAdapter(context, list) //context호출 맞나....
+
+        ft= fragmentManager?.beginTransaction()!!
 
         var presenter = GlidePresenter(this)
         //presenter.replaceAdapter(adapter) //데이터 갖고오는 함수
 //        list = presenter.getNewData()
 //        adapter.replaceAll(list)
 
-        recyclerView.layoutManager= LinearLayoutManager(context, RecyclerView.VERTICAL, false)
-
+        recyclerView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         recyclerView.adapter = adapter
 
-        connect()
+        connect() // list 받아오는 것인디..adapter연결때문에 여기 선언했는데 프레젠터로 옮길방법 찾기
 
+        //swiperefresh
+        swipeRefreshLayout = rootView.findViewById<SwipeRefreshLayout>(R.id.f_glide_srl_refreshView)
 
         return rootView
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        swipeRefreshLayout.setOnRefreshListener{
+            //새로고침할때 코드
+            adapter.replaceAll(list)
+            Log.d("refresh","replaced")
+            swipeRefreshLayout.isRefreshing = false
+        }
+
+    }
 
     companion object {
         var INSTANCE: GlideFragment? = null
@@ -86,9 +104,10 @@ class GlideFragment : Fragment(){
                 INSTANCE = GlideFragment()
             return INSTANCE!!
         }
-        
-        fun newInstance():GlideFragment {
+
+        fun newInstance(): GlideFragment {
             return GlideFragment()
         }
     }
+
 }
