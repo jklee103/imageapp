032ba6e544db4718acf883e800e00aa641ef7bba
smtownaosdev2 <smtown.aos.dev2@gmail.com>
2020-08-04 17:28:06 +0900

    Mod pagerAdapter

--- app/src/main/java/com/example/jkapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/jkapplication/view/MainActivity.kt
@@ -3,13 +3,12 @@ package com.example.jkapplication.view
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.jkapplication.R
 import com.example.jkapplication.view.adapters.PagerAdapter
-import com.example.jkapplication.view.fragments.*
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewPager: ViewPager2
@@ -22,49 +21,51 @@ class MainActivity : AppCompatActivity() {
         Fresco.initialize(this)
         setContentView(R.layout.activity_main)
 
-        var selectFragment: Fragment =
-            GlideFragment()
+//        var selectFragment: Fragment =
+//            GlideFragment()
 
-        supportFragmentManager.beginTransaction().add(R.id.a_main_fl_frame, selectFragment).commit()
+//        supportFragmentManager.beginTransaction().add(R.id.a_main_fl_frame, selectFragment).commit()
+        pagerAdapter= PagerAdapter(this,6)
         tabLayout = findViewById(R.id.a_main_tl_tab)
+        viewPager = findViewById(R.id.view_pager)
+        viewPager.adapter= pagerAdapter
+        TabLayoutMediator(tabLayout, viewPager){ tab, position ->
+            tab.text = "#${(position + 1)}"
+        }.attach()
 
-//        TabLayoutMediator(tabLayout, viewPager){ tab, position ->
-//            tab.text = "#${(position + 1)}"
-//        }.attach()
-
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-            }
-
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                var position = tab?.position
-
-                when (position) {
-                    0 -> selectFragment =
-                        GlideFragment.getInstance()
-                    1 -> selectFragment =
-                        PicassoFragment.getInstance()
-                    2 -> selectFragment =
-                        FrescoFragment.getInstance()
-                    3 -> selectFragment =
-                        ButtonFragment.getInstance()
-                    4 -> selectFragment =
-                        MoreFragment.getInstance()
-                    5 -> selectFragment =
-                        ScrollFragment.getInstance()
-                    else -> selectFragment =
-                        GlideFragment()
-
-                }
-
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.a_main_fl_frame, selectFragment).commit()
-            }
-
-        })
+//        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//            }
+//
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                var position = tab?.position
+//
+//                when (position) {
+//                    0 -> selectFragment =
+//                        GlideFragment.getInstance()
+//                    1 -> selectFragment =
+//                        PicassoFragment.getInstance()
+//                    2 -> selectFragment =
+//                        FrescoFragment.getInstance()
+//                    3 -> selectFragment =
+//                        ButtonFragment.getInstance()
+//                    4 -> selectFragment =
+//                        MoreFragment.getInstance()
+//                    5 -> selectFragment =
+//                        ScrollFragment.getInstance()
+//                    else -> selectFragment =
+//                        GlideFragment()
+//
+//                }
+//
+//                supportFragmentManager.beginTransaction()
+//                    .replace(R.id.a_main_fl_frame, selectFragment).commit()
+//            }
+//
+//        })
     }
 
 }
--- app/src/main/java/com/example/jkapplication/view/adapters/PagerAdapter.kt
+++ app/src/main/java/com/example/jkapplication/view/adapters/PagerAdapter.kt
@@ -1,27 +1,42 @@
 package com.example.jkapplication.view.adapters
 
+import android.util.Log
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.jkapplication.view.fragments.*
 
-class PagerAdapter(fm: FragmentManager, behavior: Int) : FragmentStatePagerAdapter(fm, behavior) {
-    var fm = fm
-    var behavior = behavior
-    override fun getItem(position: Int): Fragment {
+class PagerAdapter : FragmentStateAdapter{
+
+    var mCount = 0
+
+    constructor(fa: FragmentActivity?, count: Int) : super(fa!!) {
+        mCount = count
+    }
+
+    constructor(
+        fragmentManager: FragmentManager,
+        lifecycle: Lifecycle
+    ) : super(fragmentManager, lifecycle) {
+    }
+
+    override fun getItemCount(): Int {
+        return 6
+    }
+
+    override fun createFragment(position: Int): Fragment {
         when (position) {
-            0 -> return GlideFragment()
-            1 -> return PicassoFragment()
-            2 -> return FrescoFragment()
-            3 -> return ButtonFragment()
-            4 -> return MoreFragment()
-            5 -> return ScrollFragment()
-            else -> return GlideFragment()
+            0 -> return GlideFragment.getInstance()
+            1 -> return PicassoFragment.getInstance()
+            2 -> return FrescoFragment.getInstance()
+            3 -> return ButtonFragment.getInstance()
+            4 -> return MoreFragment.getInstance()
+            5 -> return ScrollFragment.getInstance()
+            else -> return GlideFragment.getInstance()
 
         }
     }
 
-    override fun getCount(): Int {
-        return behavior
-    }
 }
